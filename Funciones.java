0dba9a3cdc3fa96debe91795ed083e794d8d5d46
juan2707pog <contacto.martinez.2707@gmail.com>
2020-04-18 18:22:49 +0200

    #3 Subida Funcion2

--- Funciones.java
+++ Funciones.java
@@ -19,3 +19,42 @@ public class Funcion1 {
 		return false;
 	}
 }
+
+public class Funcion2 {
+	public static void main(String[] args) {
+		System.out.println(funcion2("A", "AAAA"));
+	}
+	public static String funcion2(String cadena1, String cadena2) {
+		if(cadena1 != null) {
+			if(cadena2 != null) {
+				if(cadena1 !="") {
+					if(cadena2 !="") {
+				int contador1 = 0;
+				int contador2 = 0;
+					for(int x=0;x<cadena1.length();x++) {
+						 if ((cadena1.charAt(x)=='a') || (cadena1.charAt(x)=='e') || (cadena1.charAt(x)=='i') || (cadena1.charAt(x)=='o') || (cadena1.charAt(x)=='u')||(cadena1.charAt(x)=='A') || (cadena1.charAt(x)=='E') || (cadena1.charAt(x)=='I') || (cadena1.charAt(x)=='O') || (cadena1.charAt(x)=='U')){ 
+						    contador1++;
+						 }
+					}
+					for(int y=0;y<cadena2.length();y++) {
+						 if ((cadena2.charAt(y)=='a') || (cadena2.charAt(y)=='e') || (cadena2.charAt(y)=='i') || (cadena2.charAt(y)=='o') || (cadena2.charAt(y)=='u')||(cadena2.charAt(y)=='A') || (cadena2.charAt(y)=='E') || (cadena2.charAt(y)=='I') || (cadena2.charAt(y)=='O') || (cadena2.charAt(y)=='U')){ 
+							contador2++;
+						 }
+					}
+				if(contador1>contador2) {
+					return("La primera cadena tiene más vocales");
+				}else if(contador1<contador2){
+					return("La segunda cadena tiene más vocales");
+				}if(contador1==contador2){
+					return("Las dos cadenas tienen el mismo numero de vocales");
+				}
+				
+				}
+		 }
+
+	}
+		}
+		return null;
+
+		}
+}
